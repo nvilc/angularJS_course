cce5b72fe73b0661fff802d16c41706d6543d906
Norvin <nvilc004@fiu.edu>
2021-10-11 21:45:28 -0400

    uncompleted first draft

---
+++ angularJS_course/module5-solution/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+  angular.module('public')
+  .controller('SignUpController', SignUpController);
+
+  function SignUpController() {
+    var signUpCtrl = this;
+
+
+  }
+
+})();
